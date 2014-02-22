86eea6ccbf3118fb120800c5838ac0cda9edb508
Daniel Bruegmann <danielbruegmann@gmx.net>
2014-02-22 05:50:01 -0800

    Changes to comments in Snapshot.

--- src/wota/gameobjects/Snapshot.java
+++ src/wota/gameobjects/Snapshot.java
@@ -2,13 +2,14 @@ package wota.gameobjects;
 
 import wota.utility.Vector;
 
-/** Snapshot of an Element of the World
- *  used to pass Instances like Ant, Hill, Sugar
- *  as Target to e.g. moveTo(Snapshot target)
+/** Snapshot of an something in the game world.
+ *  Used choose instances of Ant, Hill, Sugar
+ *  as a target when using methods like moveTo(Snapshot target).
  */
 public interface Snapshot {
 	public Vector getPosition();
 	
-	/** e.g. are the AntObjects identical for two Ants */
+	/** Every tick, a new Snapshot instance is created for each game object. 
+	 * Use this method for comparison if you saved a Snapshot instance. */
 	public boolean hasSameOriginal(Snapshot other);
 }
