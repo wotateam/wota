73752e12d4fb7f1b5f8d3ae45d0ea449f810589b
PascalCremer <pascal.cremer@gmx.de>
2014-02-27 19:55:48 +0000

    'Random' colors are maybe better than two players with the same color. #160

--- src/wota/graphics/PlayerColors.java
+++ src/wota/graphics/PlayerColors.java
@@ -4,13 +4,21 @@ import java.awt.Color;
 
 import wota.utility.Modulo;
 
+/**
+ * Helper class to generate Colors of Players.
+ */
 public class PlayerColors {
 
 	private static final Color[] playerColors = { Color.RED, Color.BLUE, Color.GREEN,
 	Color.CYAN, Color.PINK, Color.MAGENTA, Color.ORANGE, Color.YELLOW };
 
 	public static Color get(int i) {
-		return playerColors[Modulo.mod(i, PlayerColors.playerColors.length)];
+		if (i < playerColors.length) {
+			return playerColors[i];
+		}
+		else {
+			return new Color(Modulo.mod(115*i, 256), Modulo.mod(73*i, 256), Modulo.mod(192*i, 256));
+		}
 	}
 
 }
